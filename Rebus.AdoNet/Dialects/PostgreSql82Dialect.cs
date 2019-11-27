74f478187b94528d0a905b76583374b664fa1715
diego.vera <diego.vera@evicertia.com>
2019-11-27 16:52:35 +0100

    Add PostgresException on postgresExceptionNames fixing select for NoWait locking exceptions

--- Rebus.AdoNet/Dialects/PostgreSql82Dialect.cs
+++ Rebus.AdoNet/Dialects/PostgreSql82Dialect.cs
@@ -1,11 +1,14 @@
 ﻿using System;
+using System.Linq;
+using System.Collections.Generic;
 using System.Data;
 using System.Data.Common;
-
 namespace Rebus.AdoNet.Dialects
 {
 	public class PostgreSql82Dialect : PostgreSqlDialect
 	{
+		private static readonly IEnumerable<string> _postgresExceptionNames = new[] { "NpgsqlException", "PostgresException" };
+
 		protected override Version MinimumDatabaseVersion => new Version("8.2");
 		public override ushort Priority => 82;
 
@@ -15,7 +18,7 @@ namespace Rebus.AdoNet.Dialects
 
 		public override bool IsSelectForNoWaitLockingException(DbException ex)
 		{
-			if (ex != null && ex.GetType().Name == "NpgsqlException")
+			if (ex != null && _postgresExceptionNames.Contains(ex.GetType().Name))
 			{
 				var psqlex = new PostgreSqlExceptionAdapter(ex);
 				return psqlex.Code == "55P03";
