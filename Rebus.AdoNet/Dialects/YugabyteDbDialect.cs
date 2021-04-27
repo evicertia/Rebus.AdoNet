cf707fdd379d5e84fb3907266da69fc9514ea5f9
fruiz3 <fruiz@evicertia.com>
2021-04-27 13:56:52 +0200

    Add new YugabyteDb Dialect

---
+++ Rebus.AdoNet/Dialects/YugabyteDbDialect.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Data;
+using System.Linq;
+
+namespace Rebus.AdoNet.Dialects
+{
+	public class YugabyteDbDialect : PostgreSqlDialect
+	{
+		protected override Version MinimumDatabaseVersion => new Version("11.0");
+		public override bool SupportsSelectForWithSkipLocked => false;
+		public override bool SupportsSelectForWithNoWait => false;
+		public override bool SupportsTryAdvisoryLockFunction => false;
+
+		public override string GetDatabaseVersion(IDbConnection connection)
+		{
+			var result = connection.ExecuteScalar("SHOW server_version;");
+			var versionString = Convert.ToString(result);
+			return versionString.Split('-').First();
+		}
+	}
+}
